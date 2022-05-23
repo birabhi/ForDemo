00f79b9e28e10da5bdbaaaeffcc30fa105325ee8
birabhi <jamkhandi177@gmail.com>
2022-05-23 16:42:18 +0530

    For Demo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter the range ");
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        for (int i = m; i <= n; i++)
+        {
+            if(i%2==0)
+            {
+                System.out.println("Multiplication table of "+i);
+                for(int j=1;j<=10;j++)
+                {
+                    System.out.println(i + "*" + j + "=" + (i * j));
+                }
+            }
+        }
+
     }
 }
